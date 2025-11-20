8e622f0dd286e99f86d558e1cd17d0ba712fc61f
Yotam1312 <Yotam1312@gmail.com>
2025-11-20 10:25:06 +0200

    Rename the HW folder

--- HW1_JAVA_Yotam_Gefen/src/Main.java
+++ HW1_JAVA_Yotam_Gefen/src/Main.java
@@ -37,7 +37,7 @@ public class Main {
             System.out.print("Enter choice: ");
 
             choice = sc.nextInt();
-            sc.nextLine(); // Clear buffer
+            sc.nextLine();
 
             switch (choice) {
 
